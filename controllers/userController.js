0fa06d295108c576a11f896d325799a282ae69f2
ImPhatDat <phatdatn2003@gmail.com>
2023-12-06 17:41:04 +0700

    add login checking, route names changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,34 @@
 import db from '../config/firebase.js';
 import user from '../models/user.js';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, getDocs, where, getDoc } from 'firebase/firestore';
 
 const controller = {};
 
 controller.login = async (req, res) => {
     try {
-        let { userName, password } = req.body;
-        const querySnapshot = await getDocs(collection(db, 'customers'));
-        const customers = [];
-        querySnapshot.forEach((doc) => {
-            customers.push(doc.data());
-        });
+        let { username, password, isEmail } = req.body;
+        let roles = ['customers', 'merchants', 'admins'];
+        
+        let success = false;
+        for (let role in roles) {
+            const ref = collection(db, role);
+            let q;
+            if (!isEmail) {
+                q = query(ref, where("username", "==", username), 
+                where("password", "==", password));
+            }
+            else {
+                q = query(ref, where("email", "==", username), 
+                where("password", "==", password));
+            }
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+                success = true;
+                res.redirect(`/${role}`);
+            }
+        }
+
+
     } catch (error) {
         return res.status(500).json(error.message);
     }
