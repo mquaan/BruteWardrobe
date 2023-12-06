6143ce07d4bc52214ce6a530c0ba3f7760136085
Shiphu <psphu2003@gmail.com>
2023-12-06 18:56:53 +0700

    modify login page: login by user or email

--- Animated_Login_Page/LoginPage.js
+++ Animated_Login_Page/LoginPage.js
@@ -9,3 +9,39 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+
+function check_signIn_username() {
+    var username = document.getElementById("si_username").value;
+    var signIn_btn = document.getElementById("signIn_btn");
+    var regex = /[!@#$%^&*(),.?":{}|<>+=;']/;
+
+    if (regex.test(username) && !username.endsWith('@gmail.com')) {
+        errorSignInUsername.textContent = "(*) Username musn't consist special character: /[!@#$%^&*(),.?:{}|<>]/";
+        errorSignInUsername.style.display = "inline";
+        signIn_btn.disabled = true;
+
+    }
+    else {
+        errorSignInUsername.style.display = "none";
+        signIn_btn.disabled = false;
+    }
+}
+
+function check_signUp_username() {
+    var username = document.getElementById("su_username").value;
+    var signUp_btn = document.getElementById("signUp_btn");
+    var regex = /[!@#$%^&*(),.?":{}|<>+=;']/;
+
+    if (regex.test(username)) {
+        errorSignUpUsername.textContent = "(*) Username musn't consist special character: /[!@#$%^&*(),.?:{}|<>]/";
+        errorSignUpUsername.style.display = "inline";
+        signUp_btn.disabled = true;
+
+    }
+    else {
+        errorSignUpUsername.style.display = "none";
+        signUp_btn.disabled = false;
+    }
+}
