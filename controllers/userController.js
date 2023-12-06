d9921d1289c2865559df794d955820e31e70db02
ImPhatDat <phatdatn2003@gmail.com>
2023-12-06 18:05:39 +0700

    modify login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 import db from '../config/firebase.js';
-import user from '../models/user.js';
-import { collection, addDoc, getDocs, where, getDoc } from 'firebase/firestore';
+import { collection, query, getDocs, where } from 'firebase/firestore';
 
 const controller = {};
 
@@ -10,26 +9,27 @@ controller.login = async (req, res) => {
         let roles = ['customers', 'merchants', 'admins'];
         
         let success = false;
-        for (let role in roles) {
+        for (let role of roles) { // Changed to 'for...of' loop
             const ref = collection(db, role);
             let q;
             if (!isEmail) {
-                q = query(ref, where("username", "==", username), 
-                where("password", "==", password));
-            }
-            else {
-                q = query(ref, where("email", "==", username), 
-                where("password", "==", password));
+                q = query(ref, where("username", "==", username), where("password", "==", password));
+            } else {
+                q = query(ref, where("email", "==", username), where("password", "==", password));
             }
             const querySnapshot = await getDocs(q);
             if (!querySnapshot.empty) {
                 success = true;
-                res.redirect(`/${role}`);
+                // Assuming you want to set the user data to req.body
+                req.body.user = querySnapshot.docs[0].data();
+                return res.redirect(`/${role}`);
             }
         }
-
-
+        if (!success) {
+            // Handle the case where no user is found
+            res.status(404).send('User not found');
+        }
     } catch (error) {
-        return res.status(500).json(error.message);
+        return res.status(500).json({ message: error.message });
     }
 };
