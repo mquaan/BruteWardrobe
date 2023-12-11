7debdbb4d4ba296de23e1e670c1131370a7206db
mquaan <super.minhquan14@gmail.com>
2023-12-11 14:24:39 +0700

    [Home page] update feature css and handle warning

--- reactjs__mq/src/pages/Home.js
+++ reactjs__mq/src/pages/Home.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Home.css';
-import Login from './Login.js'
 import { Link } from 'react-router-dom';
 import { products } from '../helpers/product_list';
 
