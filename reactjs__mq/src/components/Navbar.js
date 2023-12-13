5dd92fee6c8ef4dec9bc02fb7cceed36e7784cd8
Shiphu <psphu2003@gmail.com>
2023-12-13 12:15:53 +0700

    update my profile animation

--- reactjs__mq/src/components/Navbar.js
+++ reactjs__mq/src/components/Navbar.js
@@ -22,6 +22,10 @@ function Navbar() {
         };
     }, []);
 
+    function ToggleMenu(subMenu){
+        subMenu.classList.toggle("open-menu");
+    }
+
     return (
         <div className={`navbar ${isNavHidden ? 'hidden' : ''}`}>
             <img src='../assets/Logo.png' className="logo" alt=""/>
@@ -34,7 +38,12 @@ function Navbar() {
                     <li><NavLink className='link' activeclassname='active' to="/cart" onClick={() => goToTop()}>
                         <i className="fa-solid fa-bag-shopping fa-bounce"></i>
                     </NavLink></li>
-                    <div className="close"><i className="far fa-times"></i></div>
+                    <li>
+                    <img src="../assets/features/avatar_cus.png" 
+                        className='user-pic' 
+                        onClick={() => ToggleMenu(document.getElementById("subMenu"))}>
+                    </img>
+                    </li>
                 </ul>
             </div>
             <div className="mobile">
@@ -42,6 +51,31 @@ function Navbar() {
                     <i className="fa-solid fa-bag-shopping"></i>
                 </NavLink>
                 <i id="bar" className="fas fa-outdent"></i>
+            </div>
+
+            <div className="sub-menu-wrap" id="subMenu">
+                <div className="sub-menu">
+                    <div className="user-info">
+                        <img src="../assets/features/avatar_cus.png" alt=""/>
+                        <h3>Phạm Sĩ Phú</h3>
+                    </div>
+
+                    <hr/>
+
+                    <a href="#" class="sub-menu-link">
+                        <img src="../assets/features/profile.png" alt=""/>
+                        <p>Edit Profile</p>
+                        <span>{'>'}</span>
+                    </a>
+
+                    <a href="#" class="sub-menu-link">
+                        <img src="../assets/features/logout.png" alt=""/>
+                        <p>Logout</p>
+                        <span>{'>'}</span>
+                    </a>
+
+                </div>
+
             </div>
         </div>
     )
